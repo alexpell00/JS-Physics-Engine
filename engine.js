31e9c48c067a21080a46ed941a776af54f01f3fd
Alex Pelletier <alexpell00@yahoo.com>
2015-05-14 09:47:14 -0600

    started structuring the physics engine function and dynamic sprite constructor

--- engine.js
+++ engine.js
@@ -2,7 +2,41 @@
 * @Author: Alex Pelletier
 * @Date:   2015-05-13 09:07:55
 * @Last Modified by:   Alex Pelletier
-* @Last Modified time: 2015-05-13 09:07:55
+* @Last Modified time: 2015-05-14 09:46:40
 */
 
 'use strict';
+
+function PhysicsEngine (options) {
+    this.objects = [];
+    this.isRunning = false;
+    this.count = 0;
+    
+
+    this.addObject = function(obj){
+    	this.objects.push(obj);
+    };
+
+    this.start = function(){
+    	this.isRunning = true;
+    	console.log("Starting");
+    	setTimeout(this.tic(), 500);
+    };
+
+    this.stop = function(){
+    	console.log("Stopping");
+    	this.isRunning = false;
+    };
+
+    this.tic = function(){
+    	if (this.isRunning == true && this.count < 300){
+    		for (var i = 0; i < this.objects.length; i++){
+    			var obj = this.objects[i];
+    			
+       		}
+
+    		this.count += 1;
+    		setTimeout(this.tic(), 500);
+    	}
+    };
+}
--- sprite.js
+++ sprite.js
@@ -2,40 +2,46 @@
 * @Author: Alex Pelletier
 * @Date:   2015-05-13 09:07:39
 * @Last Modified by:   Alex Pelletier
-* @Last Modified time: 2015-05-13 09:34:37
+* @Last Modified time: 2015-05-14 09:46:38
 */
 
 'use strict';
 
 function sprite (options) {
 				
-    var that = {};
+    var this = {};
 	
-	//set options from initialization			
-    that.context = options.context;
-    that.width = options.width;
-    that.height = options.height;
-    that.image = options.image;
-    that.x = 0;
-    that.y = 0;
-    that.xVel = 0;
-    that.yVel = 0;
-    that.xAcc = 0;
-    that.yAcc = 0;
-    that.mass = 10;
-    that.gravity = 9.8;
-    
+    //set options from initialization			
+    this.context = options.context;
+    this.width = options.width;
+    this.height = options.height;
+    this.image = options.image;
+    this.xStart = ("xStart" in options) ? options.xStart : 0;
+    this.yStart = ("yStart" in options) ? options.yStart : 0;
+    this.xVel = ("xVel" in options) ? options.xVel : 0;
+    this.yVel = ("yVel" in options) ? options.yVel : 0;
+    this.xAcc = ("xAcc" in options) ? options.xAcc : 0;
+    this.yAcc = ("yAcc" in options) ? options.yAcc : 0;
+    this.mass = ("mass" in options) ? options.mass : 10;
+    this.gravity = ("gravity" in options) ? options.gravity : false;
+    this.time = ("time" in options) ? options.time : false;
+    this.x = 0;
+    this.y = 0;
+
+    if (this.gravity){
+      this.yAcc -= 9.8;
+    }
 
     //draw the sprite
-    that.render = function () {
-        that.context.drawImage(
-           that.image,
-           that.x,
-           that.y,
-           that.width,
-           that.height);
+    this.render = function () {
+        this.context.drawImage(
+           this.image,
+           this.x,
+           this.y,
+           this.width,
+           this.height);
     };
 
-    return that;
+    return this;
 
 }
