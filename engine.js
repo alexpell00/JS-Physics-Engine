da185f1682dedad8849aee4d073406c98714bf92
Alex Pelletier <alexpell00@yahoo.com>
2015-05-15 11:11:53 -0600

    oscillation and bouncing with gravity

--- engine.js
+++ engine.js
@@ -2,41 +2,93 @@
 * @Author: Alex Pelletier
 * @Date:   2015-05-13 09:07:55
 * @Last Modified by:   Alex Pelletier
-* @Last Modified time: 2015-05-14 09:46:40
+* @Last Modified time: 2015-05-15 11:11:27
 */
 
 'use strict';
 
 function PhysicsEngine (options) {
-    this.objects = [];
-    this.isRunning = false;
-    this.count = 0;
-    
+    var that = {};
 
-    this.addObject = function(obj){
-    	this.objects.push(obj);
+    that.objects = []
+    that.isRunning = false;
+    that.count = 0;
+    that.canvas = options.canvas;
+    that.fps = 30;
+    that.timeModifier = 4;
+    that.meterToPixel = 1;
+
+    that.addObject = function(obj){
+    	that.objects.push(obj);
     };
 
-    this.start = function(){
-    	this.isRunning = true;
+    that.start = function(){
+    	that.isRunning = true;
     	console.log("Starting");
-    	setTimeout(this.tic(), 500);
+    	that.tic();
     };
 
-    this.stop = function(){
+    that.stop = function(){
     	console.log("Stopping");
-    	this.isRunning = false;
+    	that.isRunning = false;
     };
 
-    this.tic = function(){
-    	if (this.isRunning == true && this.count < 300){
-    		for (var i = 0; i < this.objects.length; i++){
-    			var obj = this.objects[i];
-    			
+    that.tic = function(interval){ //runs every frame to update
+    	if (that.isRunning == true && that.count < 300){
+    		that.canvas.getContext("2d").clearRect ( 0 , 0 , that.canvas.width, that.canvas.height );
+    		for (var i = 0; i < that.objects.length; i++){
+    			var obj = that.objects[i];
+    			obj.point = that.newPoint(obj);
+    			obj.render();
        		}
-
-    		this.count += 1;
-    		setTimeout(this.tic(), 500);
+    		that.count += 1;
     	}
+    	setTimeout(function(){that.tic(interval+1);}, 1000/that.fps);
     };
+
+    that.newPoint = function(obj){ //get new position of obj
+    	var p = point(0,0);
+    	if (obj.type == "default"){
+    		//x = x_o + v_o*t + 1/2a*t^)2
+    		p.x = this.meterToPixel*(obj.start.x + obj.vel.x*obj.time + 0.5*obj.acc.x*obj.time*obj.time); 
+			p.y = this.meterToPixel*(obj.start.y + obj.vel.y*obj.time + 0.5*obj.acc.y*obj.time*obj.time);
+    		console.log(p.y + "  " + (that.canvas.height-obj.height));
+			if (p.y > that.canvas.height-obj.height){ // check for wall collision
+				//hit on bottom
+				obj.vel.y = that.calcVel(obj).y;
+				obj.start.y = obj.point.y;
+				obj.time = 0;
+			}else if(p.x < 0`){
+				obj.vel.x *= -1;
+			}else{
+				obj.time += that.timeModifier/that.fps;
+			}
+    	}else if (obj.type == "oscillating"){
+    		// x = max*sin(freq*time+phi) + start + v_o*time
+    		p.x = obj.max.x*Math.sin(obj.freq.x*obj.time) + obj.start.x + obj.vel.x*obj.time;
+    		p.y = obj.max.y*Math.cos(obj.freq.y*obj.time) + obj.start.y + obj.vel.y*obj.time;
+    		obj.time += that.timeModifier/that.fps;
+    	}else{
+    		console.log(obj);
+    	}
+    	return p;
+    }
+
+    that.calcVel = function(obj){
+    	// v^2 = v_o^2 + 2a(x-x_o)
+    	var vel = point(0,0);
+    	vel.x = obj.COR*Math.sqrt(obj.vel.x*obj.vel.x+2*obj.acc.x*(obj.point.x-obj.start.x))
+    	vel.y = obj.COR*Math.sqrt(obj.vel.y*obj.vel.y+2*obj.acc.y*(obj.point.y-obj.start.y))
+    	return vel;
+	}
+
+    return that;
+}
+
+
+function point(x,y){
+	return {
+		x: x,
+		y: y
+	};
 }
--- sprite.js
+++ sprite.js
@@ -2,46 +2,63 @@
 * @Author: Alex Pelletier
 * @Date:   2015-05-13 09:07:39
 * @Last Modified by:   Alex Pelletier
-* @Last Modified time: 2015-05-14 09:46:38
+* @Last Modified time: 2015-05-15 11:08:29
 */
 
 'use strict';
 
+
+
 function sprite (options) {
 				
-    var this = {};
+    var that = {};
 	
     //set options from initialization			
-    this.context = options.context;
-    this.width = options.width;
-    this.height = options.height;
-    this.image = options.image;
-    this.xStart = ("xStart" in options) ? options.xStart : 0;
-    this.yStart = ("yStart" in options) ? options.yStart : 0;
-    this.xVel = ("xVel" in options) ? options.xVel : 0;
-    this.yVel = ("yVel" in options) ? options.yVel : 0;
-    this.xAcc = ("xAcc" in options) ? options.xAcc : 0;
-    this.yAcc = ("yAcc" in options) ? options.yAcc : 0;
-    this.mass = ("mass" in options) ? options.mass : 10;
-    this.gravity = ("gravity" in options) ? options.gravity : false;
-    this.time = ("time" in options) ? options.time : false;
-    this.x = 0;
-    this.y = 0;
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.start = ("start" in options) ? options.start : point(0,0);
+    that.vel = ("vel" in options) ? options.vel : point(0,0);
+    that.acc = ("acc" in options) ? options.acc : point(0,0);
+    that.mass = ("mass" in options) ? options.mass : 10;
+    that.gravity = ("gravity" in options) ? options.gravity : false;
+    that.time = ("time" in options) ? options.time : point(0,0);
+    that.COR = ("COR" in options) ? options.COR : -0.75;
+    that.type = "default"; 
+    that.point = point(0,0);
 
-    if (this.gravity){
-      this.yAcc -= 9.8;
+
+    //start doing work with the presets
+    if (that.gravity){
+      that.acc.y += 9.8;
     }
 
+
+
     //draw the sprite
-    this.render = function () {
-        this.context.drawImage(
-           this.image,
-           this.x,
-           this.y,
-           this.width,
-           this.height);
+    that.render = function () {
+        that.context.beginPath();
+        that.context.arc(that.point.x,that.point.y,10,0,2*Math.PI);
+        that.context.stroke();  
     };
 
-    return this;
+    return that;
 
+}
+
+function OscillatingSprite(options){
+  var that = sprite(options);
+
+  that.max = ("max" in options) ? options.max : point(100,100);
+  that.freq = ("freq" in options) ? options.freq : point(1,1);
+  that.type = "oscillating";
+
+  return that;
+}
+
+function point(x,y){
+  return {
+    x: x,
+    y: y
+  };
 }
